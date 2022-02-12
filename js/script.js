9129e375e91cc04cab37c30384d8b17f8d5e93bf
Alex Sevostyanov <sevoalex@mail.ru>
2022-02-12 10:53:53 +0300

    доработка модального окна

--- js/script.js
+++ js/script.js
@@ -38,14 +38,14 @@ window.addEventListener('DOMContentLoaded', function () { // сначала за
     });
     //ТАЙМЕР
 
-    const dedlain = '2022-02-10';
+    const dedlain = '2022-02-20';
 
     function getTime(endtime) { // получение разницы между датами 
         const t = Date.parse(endtime) - Date.parse(new Date()),
-            days = Math.floor( (t/(1000*60*60*24)) ), // КОЛИЧЕСТВО ДНЕЙ  //матсфлоор огругление до ближайшего целого 
-            hours = Math.floor( (t/(1000*60*60) % 24) ), // получили остаток 
-            minutes = Math.floor( (t/1000/60) % 60 ), // получили минуты 
-            seconds = Math.floor( (t/1000) % 60 ); // получили секунды
+            days = Math.floor((t / (1000 * 60 * 60 * 24))), // КОЛИЧЕСТВО ДНЕЙ  //матсфлоор огругление до ближайшего целого 
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)), // получили остаток 
+            minutes = Math.floor((t / 1000 / 60) % 60), // получили минуты 
+            seconds = Math.floor((t / 1000) % 60); // получили секунды
 
         return {
             'total': t,
@@ -55,11 +55,11 @@ window.addEventListener('DOMContentLoaded', function () { // сначала за
             'seconds': seconds
         };
     }
-function getClock(num){  // условие для того чтобы отображался дополнительный 0 
-    if ( num >= 0 && num < 10){
-        return `0${num}`; }
 
-        else {
+    function getClock(num) { // условие для того чтобы отображался дополнительный 0 
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
             return num;
 
         }
@@ -71,15 +71,16 @@ function getClock(num){  // условие для того чтобы отобр
             hours = timer.querySelector('#hours'),
             minutes = timer.querySelector('#minutes'),
             seconds = timer.querySelector('#seconds');
-        timeInterval = setInterval(updateClock, 1000);// setInterval функция котороая позваляет запускать функцию через какое то время
+        timeInterval = setInterval(updateClock, 1000); // setInterval функция котороая позваляет запускать функцию через какое то время
         updateClock();
+
         function updateClock() { // функция которая будкт обновлять наш дедлайн
             const t = getTime(endtime);
 
-            days.innerHTML = getClock (t.days);
-            hours.innerHTML = getClock (t.hours);
-            minutes.innerHTML = getClock (t.minutes);
-            seconds.innerHTML = getClock (t.seconds);
+            days.innerHTML = getClock(t.days);
+            hours.innerHTML = getClock(t.hours);
+            minutes.innerHTML = getClock(t.minutes);
+            seconds.innerHTML = getClock(t.seconds);
 
 
             if (t.total <= 0) {
@@ -89,43 +90,59 @@ function getClock(num){  // условие для того чтобы отобр
     }
     setClock('.timer', dedlain);
 
-//создание модального окна
-  
-const modalTriger = document.querySelectorAll('[data-modal]'),
-modal = document.querySelector ('.modal'),
-modalClose = document.querySelector('[data-close]');
+    //создание модального окна
 
+    const modalTriger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.modal'),
+        modalClose = document.querySelector('[data-close]');
 
-modalTriger.forEach(btn =>{  // перебирает все кнопеи с классом data-modal
-    btn.addEventListener ('click', () => {
+    function openModal() {
         modal.classList.add('show'); // класс show показать
         modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden'; //приминили свойства в теги бади хиден оно останвливает прокрутку скрлом перемешение по сайту
-        });
-});
-
-function closeModal() {
-    modal.classList.add('hide'); // класс show показать
-    modal.classList.remove('show');
-    document.body.style.overflow = '';//приминили свойствo оно возвращает в работу прокрутку скрлом перемешение по сайту
-
-}
-
-modalClose.addEventListener ( 'click',closeModal); // после клика вызовем функцию closeModal 
-
-modal.addEventListener( 'click', (e) =>{ // событие по которому кликаешь не модально окно закрывает модальное окно 
-if ( e.target === modal){ //e.target элемент события
-    closeModal();
-}
-
-});
-
-document.addEventListener( 'keydown', (e) => { // event.cod cобытие когда нажимаем на клавиатуру а имеено esc вызывается метод закрытия
-    if( e.code === 'Escape' && modal.classList.contains('show')) {//нажата ди клавиша esc и установлен ли класс show (показано ли модальное окно)
-        closeModal();
+        document.body.style.overflow = 'hidden';
+        clearInterval(closeModal);
     }
-});
 
+    modalTriger.forEach(btn => { // перебирает все кнопеи с классом data-modal
+        btn.addEventListener('click', openModal)
+    }); //приминили свойства в теги бади хиден оно останвливает прокрутку скрлом перемешение по сай;
+
+
+
+    function closeModal() {
+        modal.classList.add('hide'); // класс show показать
+        modal.classList.remove('show');
+        document.body.style.overflow = ''; //приминили свойствo оно возвращает в работу прокрутку скрлом перемешение по сайту
+
+    }
+
+    modalClose.addEventListener('click', closeModal); // после клика вызовем функцию closeModal 
+
+    modal.addEventListener('click', (e) => { // событие по которому кликаешь не модально окно закрывает модальное окно 
+        if (e.target === modal) { //e.target элемент события
+            closeModal();
+        }
+
+    });
+
+    document.addEventListener('keydown', (e) => { // event.cod cобытие когда нажимаем на клавиатуру а имеено esc вызывается метод закрытия
+        if (e.code === 'Escape' && modal.classList.contains('show')) { //нажата ди клавиша esc и установлен ли класс show (показано ли модальное окно)
+            closeModal();
+        }
+    });
+
+    const timeModal = setTimeout(openModal, 2000); // вызывает через 1 секунду функцию openmodal
+
+ function showModalScrol() {  //показ модального окна один раз 
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) { // как только пользователь долистал до конца страницы 
+                openModal(); 
+                window.removeEventListener('scroll', (showModalScrol));  // удаление события
+            }
+        })
+    };
+
+    window.addEventListener('scroll', (showModalScrol));
 
 
 });
